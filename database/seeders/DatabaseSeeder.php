5aef52376ee339e9d8402c0e5529db48eecef99b
adrianmarkj <cb009131@students.apiit.lk>
2022-07-25 10:54:04 +0530

    Testing Out User Creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // create an admin
+        \App\Models\Auth\User::create([
+            'name' => 'Administartor',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'type' => 'admin',
+        ]);
+
+        \App\Models\Auth\User::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dev', function () {
+    $admin = \App\Models\Auth\User::create([
+        'name' => 'Administartor',
+        'email' => rand(0, 100).'admin@admin.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => \Illuminate\Support\Str::random(10),
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'type' => 'admin',
+    ]);
+
+    dd($admin);
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
