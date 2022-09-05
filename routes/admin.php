6db8a16d025037bfed937326dc47c33794b14c93
adrianmarkj <cb009131@students.apiit.lk>
2022-09-05 17:39:36 +0530

    Controller Update

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
