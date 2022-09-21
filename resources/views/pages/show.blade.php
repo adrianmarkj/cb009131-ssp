bcae4ab3c1194c82a12eecfad7637fae493367fd
adrianmarkj <cb009131@students.apiit.lk>
2022-09-21 18:54:15 +0530

    Page View Edited

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -5,7 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ $page->title }}</div>
+                    <div class="card-header">
+                        <h2>{{ $page->title }}</h2>
+                    </div>
 
                     <div class="card-body text-center">
                         <img src="{{ $page->getFirstMediaUrl('images') }}" class="img-fluid mb-3" alt="{{ $page->title }}">
