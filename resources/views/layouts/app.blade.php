4c2424f00272c8720231cf6e16c1b2dc2c6d3fab
adrianmarkj <cb009131@students.apiit.lk>
2022-09-21 18:33:00 +0530

    Footer Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -155,29 +155,16 @@
         <div class="container">
             <footer class="py-5">
               <div class="row">
-                <div class="col-6 col-md-2 mb-3">
-                  <h5>Section</h5>
+                <div class="col-6 col-md-3 mb-3">
+                  <h5>Buttercup Events</h5>
                   <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    <li class="nav-item mb-2"><a href="{{ route('home') }}" class="nav-link p-0 text-muted">Home</a></li>
+                    <li class="nav-item mb-2"><a href="{{ route('archive') }}" class="nav-link p-0 text-muted">Archive</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About Us</a></li>
                   </ul>
                 </div>
 
-                <div class="col-6 col-md-2 mb-3">
-                  <h5>Section</h5>
-                  <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-                  </ul>
-                </div>
-
-                <div class="col-6 col-md-2 mb-3">
+                <div class="col-6 col-md-3 mb-3">
                   <h5>Management</h5>
                   <ul class="nav flex-column">
                     @auth('admin')
